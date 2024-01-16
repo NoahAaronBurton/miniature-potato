6658c56d9c7c7161d4ec908934097ae31be68aec
Noah Burton <nburton115@gmail.com>
2024-01-15 20:54:53 -0700

    book me bg gradient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,13 +20,13 @@ function App() {
   }
 
   return (
-  <div className='flex flex-col h-screen flex-wrap pt-5'>
+  <div className='flex flex-col h-screen flex-wrap pt-5 w-full'>
      <div className='container'>
             <h1 className='text-6xl text-balance font-bold'>Brendon Nielson</h1>
             <h2 className='text-4xl text-balance font-bold'>Commerical Audio Specialist</h2>
-            <Nav />
-            <div className='w-full h-6 bg-green-600'>
-              <p className='text-center'>Book Me</p>
+            {/* <Nav /> */}
+            <div className='w-full h-6 bg-gradient-to-r from-green-600 to-green-300'>
+              <div className='text-center w-full'>Book Me</div>
             </div>
       </div>
       
